4b7979076445a7051d67abe6b2c4d3125fad5674
R8nDr0p <rainchrstn@gmail.com>
2023-07-01 00:32:38 +0800

    change images para same size lahat ng products dapat 1000x1000

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -165,7 +165,7 @@
                             </div>
                             <div class="col">
                                 <div class="card" style="width: 18rem;">
-                                    <img src="/product_images/cpu/i5.jpg" class="card-img-top" alt="laptop.svg">
+                                    <img src="/product_images/cpu/i5.webp" class="card-img-top" alt="laptop.svg">
                                     <div class="card-body">
                                         <h5 class="card-title">Card title</h5>
                                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
