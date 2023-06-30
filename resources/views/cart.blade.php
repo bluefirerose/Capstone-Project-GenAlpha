cd027da34cfa1e8031ef0e30625dcad947aa5bfb
R8nDr0p <rainchrstn@gmail.com>
2023-07-01 01:48:01 +0800

    cart update css styles

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -48,111 +48,112 @@
                 flex-direction: row;
             }
 
-            .payment-info {
-                background: blue;
-                padding: 10px;
-                border-radius: 6px;
-                color: #fff;
-                font-weight: bold;
-            }
+        }
 
-            .product-details {
-                padding: 10px;
-            }
+        .payment-info {
+            background: #676569;
+            padding: 10px;
+            border-radius: 6px;
+            color: #fff;
+            font-weight: bold;
+        }
 
-            body {
-                background: #eee;
-            }
+        .product-details {
+            padding: 10px;
+        }
 
-            .cart {
-                background: #fff;
-            }
+        body {
+            background: #eee;
+        }
 
-            .p-about {
-                font-size: 12px;
-            }
+        .cart {
+            background: #fff;
+        }
 
-            .table-shadow {
-                -webkit-box-shadow: 5px 5px 15px -2px rgba(0, 0, 0, 0.42);
-                box-shadow: 5px 5px 15px -2px rgba(0, 0, 0, 0.42);
-            }
+        .p-about {
+            font-size: 12px;
+        }
 
-            .type {
-                font-weight: 400;
-                font-size: 10px;
-            }
+        .table-shadow {
+            -webkit-box-shadow: 5px 5px 15px -2px rgba(0, 0, 0, 0.42);
+            box-shadow: 5px 5px 15px -2px rgba(0, 0, 0, 0.42);
+        }
 
-            label.radio {
-                cursor: pointer;
-            }
+        .type {
+            font-weight: 400;
+            font-size: 10px;
+        }
 
-            label.radio input {
-                position: absolute;
-                top: 0;
-                left: 0;
-                visibility: hidden;
-                pointer-events: none;
-            }
+        label.radio {
+            cursor: pointer;
+        }
 
-            label.radio span {
-                padding: 1px 12px;
-                border: 2px solid #ada9a9;
-                display: inline-block;
-                color: #8f37aa;
-                border-radius: 3px;
-                text-transform: uppercase;
-                font-size: 11px;
-                font-weight: 300;
-            }
+        label.radio input {
+            position: absolute;
+            top: 0;
+            left: 0;
+            visibility: hidden;
+            pointer-events: none;
+        }
 
-            label.radio input:checked+span {
-                border-color: #fff;
-                background-color: blue;
-                color: #fff;
-            }
+        label.radio span {
+            padding: 1px 12px;
+            border: 2px solid #ada9a9;
+            display: inline-block;
+            color: #f1dcc9;
+            border-radius: 3px;
+            text-transform: uppercase;
+            font-size: 11px;
+            font-weight: 300;
+        }
 
-            .credit-inputs {
-                background: rgb(102, 102, 221);
-                color: #fff !important;
-                border-color: rgb(102, 102, 221);
-            }
+        label.radio input:checked+span {
+            border-color: #fff;
+            background-color: #676569;
+            color: #fff;
+        }
 
-            .credit-inputs::placeholder {
-                color: #fff;
-                font-size: 13px;
-            }
+        .credit-inputs {
+            background: #938f97;
+            color: #fff !important;
+            border-color: rgb(102, 102, 221);
+        }
 
-            .credit-card-label {
-                font-size: 9px;
-                font-weight: 300;
-            }
+        .credit-inputs::placeholder {
+            color: #fff;
+            font-size: 13px;
+        }
 
-            .form-control.credit-inputs:focus {
-                background: rgb(102, 102, 221);
-                border: rgb(102, 102, 221);
-            }
+        .credit-card-label {
+            font-size: 9px;
+            font-weight: 300;
+        }
 
-            .line {
-                border-bottom: 1px solid rgb(102, 102, 221);
-            }
+        .form-control.credit-inputs:focus {
+            background: rgb(102, 102, 221);
+            border: rgb(102, 102, 221);
+        }
 
-            .information span {
-                font-size: 12px;
-                font-weight: 500;
-            }
+        .line {
+            border-bottom: 1px solid rgb(102, 102, 221);
+        }
 
-            .information {
-                margin-bottom: 5px;
-            }
+        .information span {
+            font-size: 12px;
+            font-weight: 500;
+        }
 
-            .items {
-                -webkit-box-shadow: 5px 5px 4px -1px rgba(0, 0, 0, 0.25);
-                box-shadow: 5px 5px 4px -1px rgba(0, 0, 0, 0.08);
-            }
+        .information {
+            margin-bottom: 5px;
+        }
 
-            .spec {
-                font-size: 11px;
-            }
+        .items {
+            -webkit-box-shadow: 5px 5px 4px -1px rgba(0, 0, 0, 0.25);
+            box-shadow: 5px 5px 4px -1px rgba(0, 0, 0, 0.08);
+        }
+
+        .spec {
+            font-size: 11px;
         }
     </style>
 </head>
@@ -234,14 +235,10 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="payment-info">
-                                    <div class="d-flex justify-content-between align-items-center"><span>Card details</span><img class="rounded" src="https://i.imgur.com/WU501C8.jpg" width="30"></div><span class="type d-block mt-3 mb-1">Card type</span><label class="radio"> <input type="radio" name="card" value="payment" checked> <span><img width="30" src="https://img.icons8.com/color/48/000000/mastercard.png" /></span> </label>
+                                    <div class="d-flex justify-content-between align-items-center"><span>Card details</span><img class="rounded" src="https://i.imgur.com/WU501C8.jpg" width="30"></div><span class="type d-block mt-3 mb-1">Card type</span><label class="radio"> <input type="radio" name="card" value="payment" checked> <span><i class="fa-brands fa-cc-mastercard"></i></span> </label>
 
-                                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><img width="30" src="https://img.icons8.com/officel/48/000000/visa.png" /></span> </label>
+                                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><i class="fa-brands fa-cc-visa"></i></span> </label>
 
-                                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><img width="30" src="https://img.icons8.com/ultraviolet/48/000000/amex.png" /></span> </label>
-
-
-                                    <label class="radio"> <input type="radio" name="card" value="payment"> <span><img width="30" src="https://img.icons8.com/officel/48/000000/paypal.png" /></span> </label>
                                     <div><label class="credit-card-label">Name on card</label><input type="text" class="form-control credit-inputs" placeholder="Name"></div>
                                     <div><label class="credit-card-label">Card number</label><input type="text" class="form-control credit-inputs" placeholder="0000 0000 0000 0000"></div>
                                     <div class="row">
@@ -251,7 +248,8 @@
                                     <hr class="line">
                                     <div class="d-flex justify-content-between information"><span>Subtotal</span><span>$3000.00</span></div>
                                     <div class="d-flex justify-content-between information"><span>Shipping</span><span>$20.00</span></div>
-                                    <div class="d-flex justify-content-between information"><span>Total(Incl. taxes)</span><span>$3020.00</span></div><button class="btn btn-primary btn-block d-flex justify-content-between mt-3" type="button"><span>$3020.00</span><span>Checkout<i class="fa fa-long-arrow-right ml-1"></i></span></button>
+                                    <div class="d-flex justify-content-between information"><span>Total(Incl. taxes)</span><span>$3020.00</span></div>
+                                    <button class="btn btn-dark btn-block d-flex justify-content-between mt-3" type="button"><span>$3020.00</span><span>Checkout<i class="fa fa-long-arrow-right ml-1"></i></span></button>
                                 </div>
                             </div>
                         </div>
