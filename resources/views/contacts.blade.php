4d3e6949cdcc97623c9f99f4db3684efec9311ea
DonSal28 <130522559+DonSal28@users.noreply.github.com>
2023-07-01 13:49:21 +0800

    add contact form

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,9 +3,159 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+        * {
+            font-family: 'Poppins', sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .text-e7 {
+            color: #E7C160;
+        }
+
+        body {
+            background-color: #FBF6F1;
+        }
+
+        hr.line1 {
+            border: 10px solid #FCA75B;
+            border-radius: 5px;
+            width:80%;
+            margin: auto;
+        }
+
+        hr.line2 {
+            border: 5px solid #FCA75B;
+            border-radius: 5px;
+            width:20%;
+            margin: auto;
+        }
+
+        footer {
+            background-color: gray;
+            text-align: center;
+            margin-top : 100px;
+
+        }
+
+        @media (min-width: 768px) and (max-width: 991.98px) {
+            .row {
+                flex-direction: row;
+            }
+            
+        }
+
+        .container {
+            margin-top : 100px;
+        }
+    </style>
 </head>
 <body>
-    <h1>CONTACTS Page</h1>
+<header>
+        <nav class="navbar navbar-expand-lg ms-1" >
+            <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="gen-alpha-removebg-1.svg" style="width: 250%; height: 250%;" alt="Gen Alpha Logo">
+            </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('about') }}">About</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('products') }}">Products</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('orders') }}">Orders</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('contacts') }}"><strong>Contacts</strong></a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <hr class="line1">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <h1 class="mb-5">Contact Us</h1>
+          <form
+            action="https://formsubmit.co/3639a83e5394d40d58643c6908de4150"
+            method="POST"
+          >
+            <div class="mb-3">
+              <label class="form-label">Full Name</label>
+              <input name="name" type="text" class="form-control" required />
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input name="email" type="email" class="form-control" required />
+              <div id="emailHelp" class="form-text">
+                Don't worry, we won't share it with anyone else.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Phone (optional)</label>
+              <input name="phone" \type="tel" class="form-control" />
+              <div id="phoneHelp" class="form-text">
+                If you rather talk to a human directly.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Your Message</label>
+              <textarea
+                name="message"
+                class="form-control"
+                rows="3"
+                required
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn" style="background-color: #FCA75B;">Submit</button>
+          </form>
+        </div>
+
+        <div class="col-1"></div>
+
+        <div class="col-6 position-relative">
+          <img
+            src="mail.svg"
+            class="w-75 position-absolute top-50 start-50 translate-middle"
+          />
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <footer>
+        <h3>Group 4</h3>
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 </body>
 </html>
