723a2c74a0144b3cf1b717789648662caea890f4
bluefirerose <rose.a.exile@gmail.com>
2023-07-01 12:34:45 +0800

    Updated the pages title

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Products</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
