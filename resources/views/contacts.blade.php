a4972d0ad5689d7e7cf963cc563c63a0c9b0ba8c
DonSal28 <130522559+DonSal28@users.noreply.github.com>
2023-07-01 14:05:25 +0800

    remove unneccessary input

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
+
     <title>Home</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <style>
@@ -54,9 +54,8 @@
             margin-top : 100px;
         }
     </style>
-=======
+
     <title>Contacts</title>
->>>>>>> 56063d90c872029fb8ba2d193edd169ee04dd734
 </head>
 <body>
 <header>
