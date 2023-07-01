cc8b8bfc5d5b1b9a659a04569595b6b3828d5520
DonSal28 <130522559+DonSal28@users.noreply.github.com>
2023-07-01 13:59:26 +0800

    add header

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -3,9 +3,83 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+        * {
+            font-family: 'Poppins', sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .text-e7 {
+            color: #E7C160;
+        }
+
+        body {
+            background-color: #FBF6F1;
+        }
+
+        hr.line1 {
+            border: 10px solid #FCA75B;
+            border-radius: 5px;
+            width:80%;
+            margin: auto;
+        }
+
+        hr.line2 {
+            border: 5px solid #FCA75B;
+            border-radius: 5px;
+            width:20%;
+            margin: auto;
+        }
+
+        footer {
+            background-color: gray;
+            text-align: center;
+        }
+
+        @media (min-width: 768px) and (max-width: 991.98px) {
+            .row {
+                flex-direction: row;
+            }
+        }
+    </style>
 </head>
 <body>
-    <h1>TESTING</h1>
+<header>
+        <nav class="navbar navbar-expand-lg ms-1" >
+            <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="gen-alpha-removebg-1.svg" style="width: 250%; height: 250%;" alt="Gen Alpha Logo">
+            </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('about') }}">About</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('products') }}">Products</a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('orders') }}"><strong>Orders</strong></a>
+                        </li>
+                        <li class="nav-item px-lg-3">
+                            <a class="nav-link active" href="{{ url('contacts') }}">Contacts</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <hr class="line1">
 </body>
 </html>
