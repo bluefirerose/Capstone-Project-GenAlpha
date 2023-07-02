6af0a70c9d2be5d4c5afb4071ef82c36e1ebd6e4
R8nDr0p <rainchrstn@gmail.com>
2023-07-03 00:57:40 +0800

    added breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,80 +14,18 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/products', function () {
-    return view('products');
-});
-
-Route::get('/orders', function () {
-    return view('orders');
-});
-
-Route::get('/contacts', function () {
-    return view('contacts');
-});
-
-Route::get('/cart', function () {
-    return view('cart');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/userhome', function () {
-    return view('userhome');
-});
-
-Route::get('/admin', function () {
-    return view('admin');
-});
-
-Route::get('/adminproduct', function () {
-    return view('adminproduct');
-});
-
-Route::get('/adminlist', function () {
-    return view('adminlist');
-});
-
-Route::get('/adminmessage', function () {
-    return view('adminmessage');
-});
-
-Route::get('/adminprofile', function () {
-    return view('adminprofile');
-});
-
-Route::get('/adminusers', function () {
-    return view('adminusers');
-});
-
-Route::group(['middleware' => 'guest'], function () {
-    Route::get('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
-});
- 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/userhome', [HomeController::class, 'index']);
-    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
-});
-
-
-
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-
+require __DIR__.'/auth.php';
