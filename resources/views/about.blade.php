adcb0e89b2be7bb4066cea0f8584d97bec9ccdf7
DonSal28 <130522559+DonSal28@users.noreply.github.com>
2023-07-04 10:32:07 +0800

    add image donald about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -271,8 +271,8 @@
   </div>
   <div class="col-md-4">
     <div class="thumbnail">
-        <img src="pc.svg" alt="Donald" style="width:70%">
-        <div class="caption ">
+        <img src="donald.png" alt="Donald" style="width:70%">
+        <div class="caption m-3">
         <h5><bold>Donald</bold></h5>
           <small>Full-Stack Developer</small>
         </div>
